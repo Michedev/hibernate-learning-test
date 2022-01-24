78f8f11caa308067a591f19317f8ac725bab259f
mikedev <mik3dev@gmail.com>
2022-01-24 17:59:03 +0100

    rename HibernateLearnTest.java to HibernateLearnTestTask.java because tests relate only to Task class

--- src/test/java/edu/mikedev/hibernate_learning_test/HibernateLearnTestTask.java
+++ src/test/java/edu/mikedev/hibernate_learning_test/HibernateLearnTestTask.java
@@ -21,7 +21,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class HibernateLearnTest {
+public class HibernateLearnTestTask {
 	
 	Path testResourceDirectory;
 	File hibernateConfigFile;
